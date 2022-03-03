c3fe3632f8544998492be6575051086caf2a503d
markalabastro <42789917+markalabastro@users.noreply.github.com>
2022-03-03 17:45:30 -0600

    Change variable names and refactor code

--- scripts.js
+++ scripts.js
@@ -4,51 +4,58 @@
 //create another loop within that loop that creates the vertical divs
 //use the width and length values to determine the dimensions of the grid
 //create a loop that adds the mouse over event listener to each div so that it adds the class color
-//create a button that clears the color class 
+//create a button that clears the color class
 
 const width = document.querySelector("#width");
 const height = document.querySelector("#height");
 const submitButton = document.querySelector("#generate");
 const resetButton = document.querySelector("#reset");
 
-
-
-submitButton.addEventListener('click', (e) => {
-
+function removeBoxes() {
   const boxes = document.querySelectorAll(".box");
-  for (const box of boxes){
+  for (const box of boxes) {
     box.remove();
   }
+}
 
+function removeColor() {
+  const boxes = document.querySelectorAll(".box");
+  for (const box of boxes) {
+    box.classList.remove("color");
+  }
+}
+
+function createGrid() {}
+
+submitButton.addEventListener("click", (e) => {
   e.preventDefault();
+  removeBoxes();
 
-  let widthBoxes = width.value;
-  let heightBoxes = height.value;
+  let numWidthBoxes = width.value;
+  let numHeightBoxes = height.value;
 
-  let widthBoxesDim = 898 / widthBoxes;
-  let heightBoxesDim = 898 / heightBoxes;  
+  let widthBoxDim = 900 / numWidthBoxes;
+  let heightBoxDim = 900 / numHeightBoxes;
   let grid = document.querySelector(".grid");
 
-
-  for (i=1; i<=widthBoxes; i++){
-    for (j=1; j<=heightBoxes; j++){
+  for (i = 1; i <= numHeightBoxes; i++) {
+    for (j = 1; j <= numWidthBoxes; j++) {
       let box = document.createElement("div");
-      box.classList.add("box")
-      box.setAttribute("style", `display:flex; width: ${widthBoxesDim}px; height: ${heightBoxesDim}px; flex:0 1 1`);
+      box.classList.add("box");
+      box.setAttribute(
+        "style",
+        `display:flex; width: ${widthBoxDim}px; height: ${heightBoxDim}px; `
+      );
       box.addEventListener("mouseover", () => {
-        box.classList.add("color")
-      })
+        box.classList.add("color");
+      });
       grid.appendChild(box);
     }
   }
-})
+  grid.style.border = 0;
+});
 
-resetButton.addEventListener('click', (e) => {
+resetButton.addEventListener("click", (e) => {
   e.preventDefault();
-
-  const boxes = document.querySelectorAll(".box");
-  for (const box of boxes){
-    box.classList.remove("color");
-  }
-})
-
+  removeColor();
+});
